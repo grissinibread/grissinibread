c454b48c89c9e10e3acf23d4c17262d764a1c0a3
Alejandro Navarro <alejandronavarro@Mac.attlocal.net>
2025-04-22 01:52:14 -0700

    reverting changes

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    root: 'src', // if you're using src as your working directory
-    build: {
-        outDir: '../dist'
-    }
-});
